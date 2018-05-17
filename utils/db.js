7a4e57d7d7a59c8dff0850167bdc3206ad65b28a
Kenny Wibowo <kewibowo@ucsd.edu>
2018-05-17 00:04:46 -0700

    Implement most of authentication flow, some issues with serialization

--- utils/db.js
+++ utils/db.js
@@ -9,34 +9,34 @@ const db = new Sequelize('database', null, null, {
   storage: ':memory:', // For now, store in memory
 })
 
+// Database population
 db.authenticate()
   .then(() => {
-    console.log('Database connection has been established successfully');
-  }, (err) => {
-    console.log('Unable to connect to the database:', err);
-  });
-
-// Database population
-db.query("create table if not exists Role (" +
-  "id integer primary key autoincrement," +
-  "role_name string not null unique);")
-  .then(() => {
-
+      console.log('Database connection has been established successfully');
+      // Run all of the schema 'create table' population at the same time
+      return Promise.all([
+        db.query("create table if not exists Role (" +
+                 "id integer primary key autoincrement," +
+                 "role_name string not null unique);"),
+        db.query("create table if not exists User (" +
+                 "id integer primary key autoincrement," +
+                 "username string not null unique," +
+                 "password string not null," +
+                 "role_id integer references Role(id));")]);
+    }, (err) => {
+      console.log('Unable to connect to the database:', err);
+  }).then((queryResults) => {
     // Populate roles if not done so already
-    db.query('select role_name from Role')
-      .then((queryResult) => {
-        let roles = queryResult[0]
+    return db.query('select role_name from Role')
+  })
+  .then((queryResult) => {
+    let roles = queryResult[0]
 
-        if(!roles || roles.length == 0) {
-          db.query('insert into Role (role_name) values ("candidate"), ("administrator");');
-        }
-      });
+    if(!roles || roles.length == 0) {
+      db.query('insert into Role (role_name) values ("candidate"), ("administrator");');
+    }
   });
 
-db.query("create table if not exists User (" +
-  "id integer primary key autoincrement," +
-  "username string not null unique," +
-  "password string not null," +
-  "role integer references Role(id));");
+
 
 module.exports = db;
