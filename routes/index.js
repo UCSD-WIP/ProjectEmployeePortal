de53b0887eec6fb4c76c693da71359263f905b80
Kenny Wibowo <kewibowo@ucsd.edu>
2018-05-16 17:49:16 -0700

    Implement Sequelize interactions with sqlite3 database

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,20 @@
 var express = require('express');
+var db = require('../utils/db.js');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Title' });
+});
+
+/* GET login page */
+router.get('/login', function(req, res, next) {
+  res.render('login');
+});
+
+/* GET register page */
+router.get('/register', function(req, res, next) {
+  res.render('register');
 });
 
 module.exports = router;
