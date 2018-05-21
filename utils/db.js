4c0e293f2cc0ca8ad4b919037eb49b79caa21fa0
Taylor Lew <taylorlew@Taylors-MacBook-Pro-2.local>
2018-05-21 15:32:16 -0700

    Adds table for stories

--- utils/db.js
+++ utils/db.js
@@ -23,15 +23,23 @@ db.authenticate()
         "username string not null unique," +
         "password string not null," +
         "role_id integer references Role(id));"),
-      db.query("create table if not exists Jobs (" +
-      "id integer primary key autoincrement," +
-      "company_name string not null unique," +
-      "position string not null," +
-      "job_field string not null," +
-      "location string not null," +
-      "timestamp datetime not null," +
-      "content string not null," +
-      "email string not null);")
+      db.query("create table if not exists Job (" +
+        "id integer primary key autoincrement," +
+        "company_name string not null unique," +
+        "position string not null," +
+        "job_field string not null," +
+        "location string not null," +
+        "timestamp datetime default current_timestamp," +
+        "content string not null," +
+        "email string not null);"),
+      db.query("create table if not exists Story (" +
+        "id integer primary key autoincrement," +
+        "image string," +
+        "title string not null," +
+        "description string not null," +
+        "timestamp datetime default current_timestamp," +
+        "content string not null," +
+        "featured integer not null);")
     ]);
   }).then((queryResults) => {
     // Populate roles if not done so already
