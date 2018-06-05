ec709e8903d105003a63b69472493b7531c8282a
Carlos Chulo <thecompscientist.dev@gmail.com>
2018-06-05 13:27:22 -0700

    fixed admin script

--- auto-admin-create.js
+++ auto-admin-create.js
@@ -1,7 +1,8 @@
 const request = require('request');
 
 // submit post request to localhost:3000/register-admin
-request.post({
+setTimeout(() => {
+  request.post({
     url: 'http://localhost:3000/register-admin',
     json: true,
     body: {
@@ -11,9 +12,10 @@ request.post({
     }
   }, (error, response, body) => {
     if (!error && response.statusCode == 200) {
-        console.log("\n",body) // Show the HTML for the Google homepage. 
-      }
-      else {
-        console.log("Error "+response.body)
-      }
-  });
+      console.log("\n", body) // Show the HTML for the Google homepage. 
+    }
+    else {
+      console.log("Error " + response.body)
+    }
+  })
+}, 5);
